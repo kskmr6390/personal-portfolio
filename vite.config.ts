0675f401ca40db0ee5ff3818bbd5772e963bf3e4
satyam <skmr6390@gmail.com>
2025-04-11 04:40:12 +0530

    index-update

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/personal-portfolio/',
+  base: mode === 'production' ? '/personal-portfolio/' : '/',
   server: {
     host: "::",
     port: 8080,
